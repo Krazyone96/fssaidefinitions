2d81c497e60eacce75eb1b37b21f5af58587b663
Krazyone96 <70380221+Krazyone96@users.noreply.github.com>
2022-05-18 16:43:49 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,41 +2,731 @@
 
 let data = [
 	{
-	numb: 1,
-	question: "What is the fat content of Low Fat Cream?",
-	answer: "25.0%",
+numb: 1,
+	question: "_____ means the residual material left over when oil is extracted by a solvent from any oil bearing material",
+	answer: "De-oiled meal",
 	options: [
-		"15.0%",
-		"25.0%",
-		"40.0%",
-		"60.0%",
+		"De-fatted meal",
+		"Protein meal",
+		"De-oiled meal",
+		"Protein cake",
 		"None of the above"
 	]
-	},
-	{
-	numb: 2,
-	question: "What is the fat content of Medium Fat Cream?",
-	answer: "40.0%",
+},
+{
+numb: 2,
+	question: "______ means the product prepared by admixture of cow-milk or buffalo-milk or both with fresh skimmed milk or by admixture of cow or buffalo milk or both that has been standardised to fat and solids non-fat as per regulations",
+	answer: "Double Toned Milk",
 	options: [
-		"40.0%",
-		"15.0%",
-		"25.0%",
-		"60.0%",
+		"Toned Milk",
+		"Double Toned Milk",
+		"Skimmed Milk",
+		"Recombined Milk",
 		"None of the above"
 	]
-	},
-	{
-	numb: 3,
-	question: "What is the fat content of High Fat Cream?",
-	answer: "60.0%",
+},
+{
+numb: 3,
+	question: "Double Toned Milk means the product prepared by ",
+	answer: "All the above",
 	options: [
-		"25.0%",
-		"60.0%",
-		"40.0%",
-		"15.0%",
+		"Toned Milk",
+		"admixture of cow-milk or buffalo-milk or both with fresh skimmed milk",
+		"admixture of cow or buffalo milk or both that has been standardised to fat and solids non-fat as per regulations",
+		"pasteurised and shall show a negative phosphatase test",
+		"All the above"
+	]
+},
+
+{
+numb: 4,
+	question: "When fat or non-fat milk solids are used to prepare Double Toned Milk, it shall be ensured that the product remains __________ and no deposition of solids take place on standing",
+	answer: "homogenous",
+	options: [
+		"homogenous",
+		"hetergenous",
+		"suspended",
+		"emulsified",
 		"None of the above"
 	]
-	},
+},
+
+{
+numb: 5,
+	question: "_______ means the process of addition of hydrogen to an edible vegetable oil using a catalyst to produce a fat with semi-solid consistency",
+	answer: "Hydrogenation",
+	options: [
+		"Reduction",
+		"Substitution",
+		"Oxidation",
+		"Hydrogenation",
+		"None of the above"
+]
+},
+
+{
+numb: 6,
+	question: "Hydrogenation means the process of _______ to an edible vegetable oil using a catalyst to produce a fat with semi-solid consistency",
+	answer: "addition of hydrogen",
+	options: [
+		"removal of oxygen",
+		"addition of hydrogen",
+		"formation of double bond",
+		"removal of hydrogen",
+		"None of the above"
+]
+},
+
+{
+numb: 7,
+	question: "_______, may contain nuts (whole,fragmented or ground), chocolate, coffee or any other edible flavour, edible food colour and cane sugar",
+	answer: "Flavoured milk",
+	options: [
+		"Flavoured milk",
+		"Toned milk",
+		"Skimmed milk",
+		"Recombined milk",
+		"None of the above"
+]
+},
+
+{
+numb: 8,
+	question: "Flavoured milk shall be _________",
+	answer: "All the above",
+	options: [
+		"Pasteurised",
+		"Sterilised",
+		"Boiled",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 9,
+	question: "Flavoured milk may ________",
+	answer: "All the above",
+	options: [
+		"contain nuts (whole, fragmented or ground), chocolate, coffee or any other edible flavour, edible food colours and cane sugar",
+		"shall be pasteruised, sterilised and boiled",
+		"shall be mentioned on the label",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 10,
+	question: "_____ means milk or a combination of buffalo or cow milk or a product prepared by a combination of both that has been standardised to fat and solids non fat percentage by adjustment/addition of milk solids to meet the regulations",
+	answer: "Full Cream Milk",
+	options: [
+		"Double Toned Milk",
+		"Skimmed Milk",
+		"Full Cream Milk",
+		"Pasteurised Milk",
+		"None of the above"
+]
+},
+
+{
+numb: 11,
+	question: "Full cream milk shall be _______",
+	answer: "pasteurised",
+	options: [
+		"sterilised",
+		"boiled",
+		"pasteurised",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 12,
+	question: "Full cream milk means _______",
+	answer: "All the above",
+	options: [
+		"means milk or a combination of buffalo or cow milk or a product prepared by a combination of both that has been standardised to fat and solids non fat percentage by adjustment/addition of milk solids to meet the regulations",
+		"shall be pasteurised and shall show a negative Phosphatase test",
+		"shall be packed in clean, sound and sanitary containers properly sealed to prevent contamination",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 13,
+	question: "______ means any physical procedure, involving the intentional exposure of food to ionizing radiation",
+	answer: "Irradiation",
+	options: [
+		"Pasteurisation",
+		"Irradiation",
+		"Homogenisation",
+		"Purification",
+		"None of the above"
+]
+},
+
+{
+numb: 14,
+	question: "______ means any facility which is capable of being utilized for treatment of food by irradiation",
+	answer: "Irradiation facility",
+	options: [
+		"Irradiation facility",
+		"Nuclear facility",
+		"Atomica facility",
+		"Bombardment facility",
+		"None of the above"
+]
+},
+
+{
+numb: 15,
+	question: "Irradiated food means articles of food subjected to radiation by",
+	answer: "All the above",
+	options: [
+		"Gamma rays",
+		"X-rays generated from machine sources operated at or below an energy level of 5 million eV",
+		"Sub-atomic particles, namely electrons generated from machine sources operated at or below an energy levle of 10 million eV",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 16,
+	question: "Irradiated food means articles of food subjected to radiation by X-rays generated from machine sources operated at or below an energy levle of",
+	answer: "5 million eV",
+	options: [
+		"10 million eV",
+		"10 eV",
+		"5 million eV",
+		"5 eV",
+		"None of the above"
+]
+},
+
+{
+numb: 17,
+	question: "Irradiated food means articles of food subjected to radiation by ________ generated from machine sources operated at or below an energy levle of 5 million eV",
+	answer: "X-ray",
+	options: [
+		"electrons",
+		"X-ray",
+		"neutrons",
+		"protons",
+		"None of the above"
+]
+},
+
+{
+numb: 18,
+	question: "Irradiated food means articles of food subjected to radiation by _________ generated from machine sources operated at or below an energy level of 10 million eV",
+	answer: "Sub-atomic particles, namely electron",
+	options: [
+		"Sub-atomic particles, namely proton",
+		"Sub-atomic particles, namely neutron",
+		"Sub-atomic particles, namely electron",
+		"X-rays",
+		"None of the above"
+]
+},
+
+{
+numb: 19,
+	question: "Irradiated food means articles of food subjected to radiation by sub-atomic particles, namely electrons generated from machine sources operated at or below an energy level of _________",
+	answer: "10 million eV",
+	options: [
+		"10 million eV",
+		"10 eV",
+		"5 million eV",
+		"5 eV",
+		"None of the above"
+]
+},
+
+{
+numb: 20,
+	question: "The dose levels for irradiation of food is specified in __________ of the Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+	answer: "Schedule I",
+	options: [
+		"Schedule I",
+		"Schedule II",
+		"Schedule III",
+		"Schedule IV",
+		"None of the above"
+]
+},
+
+{
+numb: 21,
+	question: "The dose levels of irradiation of food is specified in _________",
+	answer: "Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+	options: [
+		"Food Irradiation Rules, 1991",
+		"Atomic Energy (Control of Irradiation of Food) Rules, 1992",
+		"Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+		"Food Irradiation Rules, 1992",
+		"None of the above"
+]
+},
+
+{
+numb: 22,
+	question: "_____ is the normal mammary secretion derived from complete milking of healthy milch animal without either addition thereto or extraction therefrom unless otherwise provided in the regulations",
+	answer: "Milk",
+	options: [
+		"Colostrum",
+		"Milk",
+		"Fat",
+		"Whey",
+		"None of the above"
+]
+},
+
+{
+numb: 23,
+	question: "Milk shall be free from _______",
+	answer: "Colostrum",
+	options: [
+		"Fat",
+		"Whey",
+		"Colostrum",
+		"Casein",
+		"None of the above"
+]
+},
+
+{
+numb: 24,
+	question: "Total urea content in milk shall not be more than _______",
+	answer: "700 ppm",
+	options: [
+		"200 ppm",
+		"600 ppm",
+		"500 ppm",
+		"700 ppm",
+		"None of the above"
+]
+},
+
+{
+numb: 25,
+	question: "______ means a combination of milk of cow, buffalo, sheep, goat or any other milch animal and may be a combination of any of these milk which has been made and conforms to the standards",
+	answer: "Mixed milk",
+	options: [
+		"Toned milk",
+		"Double Toned milk",
+		"Mixed milk",
+		"Skimmed milk",
+		"None of the above"
+]
+},
+
+{
+numb: 26,
+	question: "_____ shall not contain any substance not found in milk unless specified in standards",
+	answer: "Milk products",
+	options: [
+		"Non-milk products",
+		"Milk products",
+		"Vegan products",
+		"Soy products",
+		"None of the above"
+]
+},
+
+{
+numb: 27,
+	question: "_____ means an emulsion of oils and fats in water",
+	answer: "Margarine",
+	options: [
+		"Lard",
+		"Margarine",
+		"Milk",
+		"Cream",
+		"None of the above"
+]
+},
+
+{
+numb: 28,
+	question: "Margarine is a/an __________",
+	answer: "Emulsion",
+	options: [
+		"Suspension",
+		"Emulsion",
+		"Colloid",
+		"Solution",
+		"None of the above"
+]
+},
+
+{
+numb: 29,
+	question: "Pasteurisation involves heating every part of milk to _________",
+	answer: "All the above",
+	options: [
+		"63 deg C for at least 30 minutes",
+		"71.5 deg C for 15 seconds",
+		"135 deg C for 2-5 seconds",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb:30,
+	question: "Pasteurisation involves heating every part of milk to __________",
+	answer: "All the above",
+	options: [
+		"280-302 deg F for 2-5 seconds",
+		"161 deg F for 15 seconds",
+		"145.4 deg F for 30 minutes",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 31,
+	question: "Pasteruisation will show a ________",
+	answer: "negative Phosphatase test",
+	options: [
+		"Positive Phosphatase test",
+		"Negative Phosphatase test",
+		"Positive Catalase test",
+		"Negatice Catalase test",
+		"None of the above"
+]
+},
+
+{
+numb: 32,
+	question: "All pasteurised milk of different class shall be cooled immediately to a temperature of _____",
+	answer: "10 deg C or less",
+	options: [
+		"10 deg C",
+		"15 deg C",
+		"10 deg C or less",
+		"15 deg C or less",
+		"None of the above"
+]
+},
+
+{
+numb: 33,
+	question: "_____ means any person appointed as such by license who satisfied the qualifications and requirements as for training specified in Schedule II of the Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+	answer: "Operator of Irradiation Facility",
+	options: [
+		"Operator of Bombardment Faciltiy",
+		"Operator of Irradiation Facility",
+		"Scientist of Irradiation Facility",
+		"Scientist of Bombardment Facility",
+		"None of the above"
+]
+},
+
+{
+numb: 34,
+	question: "The requirements and training for operator of Irradiation facility is given in _______",
+	answer: "Schedule II of Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+	options: [
+		"Schedule I of Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+		"Schedule II of Atomic Energy (Control of Irradiation of Food) Rules, 1991",
+		"Schedule II of Irradiation of Food Rules, 1992",
+		"Schedule II of Irradiation of Food Rules, 1991",
+		"None of the above"
+]
+},
+
+{
+numb: 35,
+	question: "____ means the homogenised product prepared from milk fat, non-fat milk solids and water",
+	answer: "Recombined milk",
+	options: [
+		"Double Toned milk",
+		"Full cream milk",
+		"Recombined milk",
+		"Skimmed milk",
+		"None of the above"
+]
+},
+
+{
+numb: 36,
+	question: "Recombined milk means the _______ product prepared from milk fat, non-fat milk solids and water",
+	answer: "homogenised",
+	options: [
+		"reconstituted",
+		"blended",
+		"filtered",
+		"homogenised",
+		"None of the above"
+]
+},
+
+{
+numb: 37,
+	question: "Recombined milk means the _______",
+	answer: "homogenised product prepared from milk fat, non-fat milk solids and water",
+	options: [
+		"shall be pasteurised and show a negative phosphatase test",
+		"homogenised product prepared from milk fat, non-fat milk solids and water",
+		"shall be pasteurised, sterilised or boiled",
+		"Both a & b",
+		"None of the above"
+]
+},
+
+{
+numb: 38,
+	question: "_____ means any vegetable oil which is obtained by expression or solvent extraction of vegatble oil bearing materials, deacidified with alkali and/ or by physical refining and/or by miscella refining using permitted food grade solvents and/or degumming followed by bleaching with absorbent earth and/or carbon and deodorized with steam without using any chemical agents",
+	answer: "Refined vegetable oil",
+	options: [
+		"Cold pressed oil",
+		"Refined vegetable oil",
+		"Natural oil",
+		"Purified oil",
+		"None of the above"
+]
+},
+
+{
+numb: 39,
+	question: "_____ means a process by which an expressed vegetable oil or a solvent-extracted oil is deacidified",
+	answer: "Refining",
+	options: [
+		"Refining",
+		"Salting",
+		"Ozonification",
+		"Purifying",
+		"None of the above"
+]
+},
+
+{
+numb: 40,
+	question: "Refining means a process by which an expressed vegetable oil or a solvent-extracted oil is _______",
+	answer: "deacidified",
+	options: [
+		"hydrogenated",
+		"filtered",
+		"deacidified",
+		"fortified",
+		"None of the above"
+]
+},
+
+{
+numb: 41,
+	question: "Refinig means a process by which an expressed vegetable oil or a solvent extracted oil is deacidified by ______",
+	answer: "All the above",
+	options: [
+		"alkali",
+		"physical refining",
+		"miscella refining",
+		"Process of degumming",
+		"All the above"
+]
+},
+
+{
+numb: 42,
+	question: "_____ is done using permitted food grade solvent followed by bleaching with absorbent Earth and/or activated carbon or both of them and deodorized with steam without using any other chemical agent",
+	answer: "Miscella refining",
+	options: [
+		"Physical refining",
+		"Chemical refining",
+		"Safe refining",
+		"Miscella refining",
+		"None of the above"
+]
+},
+
+{
+numb: 43,
+	question: "The process of degumming is done using _________",
+	answer: "Both a and b",
+	options: [
+		"Phosphoric acid",
+		"Citric acid",
+		"Only a",
+		"Both a and b",
+		"None of the above"
+]
+},
+
+{
+numb: 44,
+	question: "______ means the product from milk from which almost all the milk fat has been removed mechanically",
+	answer: "Skimmed milk",
+	options: [
+		"Full cream milk",
+		"Skimmed milk",
+		"Toned milk",
+		"Recombined milk",
+		"None of the above"
+]
+},
+
+{
+numb: 45,
+	question: "_______ means heating milk in sealed container continuously to a temperature of either 115 deg C for 15 minutes or at least 130 deg C for a period of one second in continous flow",
+	answer: "Sterilisation",
+	options: [
+		"Sterilisation",
+		"Pasteurisation",
+		"Boiling",
+		"Ultra filtration",
+		"None of the above"
+]
+},
+
+{
+numb: 46,
+	question: "Sterilisation means heating milk in sealed container continuously to a temperature of _______",
+	answer: "Both a and b",
+	options: [
+		"115 deg C for 15 minutes",
+		"130 deg C for 1 second or more",
+		"Only b",
+		"Both a and b",
+		"None of the above"
+]
+},
+
+{
+numb: 47,
+	question: "Sterilisation means _________",
+	answer: "All the above",
+	options: [
+		"heating milk in sealed container continously to a temperature of either 115 deg C for 15 minutes or 130 deg C for 1 second or more",
+		"packed under aseptic condition in hermetically sealed container",
+		"preservation at room temperature for a period not less than 15 days from manufacturing date",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 48,
+	question: "_______ means any vegatable oil obtained from oil bearing material by the process of extraction by a solvent",
+	answer: "Solvent extracted oil",
+	options: [
+		"Solvent extracted edible flour",
+		"Solvent extracted oil",
+		"Chemically extracted oil",
+		"Cold pressed oil",
+		"None of the above"
+]
+},
+
+{
+numb: 49,
+	question: "______ means the ground material obtained from specially prepared deoiled meal, that is the residual material left over when oil is extracted by a solvent from an oil cake immediately following single pressing of good quality edible oilseeds",
+	answer: "Solvent extracted edible flour",
+	options: [
+		"Solvent extracted edible flour",
+		"COld pressed edible flour",
+		"Deoiled meal",
+		"Defatted meal",
+		"None of the above"
+]
+},
+
+{
+numb: 50,
+	question: "Solvent extracted flour is the residual material left over when oil is extracted by a solvent from oil cake following the _______ of good quality edible seeds",
+	answer: "Single-pressing",
+	options: [
+		"Repeated-pressing",
+		"Double-pressing",
+		"Single-pressing",
+		"Both a and b",
+		"None of the above"
+]
+},
+
+{
+numb: 51,
+	question: "_____ means the product prepared by admixture of cow or buffalo milk or both with fresh skimmed milk or by admixture of cow or buffalo milk or both that has been standardised to fat and solids non-fat percentage as per regulation",
+	answer: "Toned milk",
+	options: [
+		"Skimmed milk",
+		"Toned milk",
+		"Full cream milk",
+		"Recombined milk",
+		"None of the above"
+]
+},
+
+{
+numb: 52,
+	question: "Toned milk means the product prepared by __________",
+	answer: "All the above",
+	options: [
+		"Admixture of cow or buffalo milk or both with fresh skimmed milk",
+		"Admixture of cow or buffalo milk or both that has been standardised to fat and solids non-fat percentage by adjustment of milk solids",
+		"shall be pasteurised and shall show a negative phosphatase test",
+		"when fat and non-fat milk solids are used it shall be ensured that the product remains homogenous and no deposition of solids takes place on standing",
+		"All the above"
+]
+},
+
+{
+numb: 53,
+	question: "_____ means oils produced from oil cakes or oil seeds or oil bearing materials of plant origin and containing glycerides",
+	answer: "Vegetable oil",
+	options: [
+		"Vegan oil",
+		"vegetable oil",
+		"Both a and b",
+		"Mineral oil",
+		"None of the above"
+]
+},
+
+{
+numb: 54,
+	question: "Vegetable oil means oil produced from oil cakes or oil seeds or oil bearing materials of plant origin and containing",
+	answer: "glycerides",
+	options: [
+		"emulsifiers",
+		"lecithin",
+		"protein",
+		"glycerides",
+		"None of the above"
+]
+},
+
+{
+numb: 55,
+	question: "______ means any product obtained for edible purposes by subjecting one or more edible oils to any or a combination of processes such as refining, blending, hydrogenation or interesterification or winterization",
+	answer: "Vegetable oil product",
+	options: [
+		"Vegetable oil product",
+		"Animal oil product",
+		"Mineral oil product",
+		"All the above",
+		"None of the above"
+]
+},
+
+{
+numb: 56,
+	question: "Vegetable oil product means any product obtained for edible purposes by subjecting one or more edible oil to ______",
+	answer: "All the above",
+	options: [
+		"refining",
+		"blending",
+		"hydrogenation",
+		"interesterification or winterization",
+		"All the above"
+]
+},
+	
 ];
 
 
